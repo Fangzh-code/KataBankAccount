6715bed8fffa26cc35b726525ac197da9173b358
Fangzh-code <zhouheng.fang1989@gmail.com>
2019-07-15 00:03:06 +0200

    update main

--- src/main/java/com/bank/account/Main.java
+++ src/main/java/com/bank/account/Main.java
@@ -1,10 +1,24 @@
 package com.bank.account;
 
+import java.util.ArrayList;
+
+import com.bank.account.exception.BankAccountException;
+import com.bank.account.operation.OperationType;
+import com.bank.account.statement.Statement;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws BankAccountException {
+		Account account = new Account(0, new ArrayList<Statement>());
+		
+		account.doOperation(OperationType.DEPOSIT, 1500);
+		account.doOperation(OperationType.DEPOSIT, 500);
+		account.doOperation(OperationType.DEPOSIT, 2500);
+		account.doOperation(OperationType.WITHDRAWAL, 3500);
+		
+		account.historyPrint();
 
+		System.out.println("Account balance : " + account.getAccountBalance());
 	}
 
 }
